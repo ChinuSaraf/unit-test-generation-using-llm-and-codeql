d7e3f97dbd9cb29f6855b654f24dbdb9e5f74672
Kedar <kedar.takwane@gmail.com>
2023-10-26 19:24:11 -0500

    randoop files

---
+++ randoop-unit-test-generation/examples/WhoClientTest.java
@@ -0,0 +1,28 @@
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RegressionTest0 {
+
+    public static boolean debug = false;
+
+    @Test
+    public void test1() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test1");
+        org.apache.hadoop.security.authentication.examples.WhoClient whoClient0 = new org.apache.hadoop.security.authentication.examples.WhoClient();
+        java.lang.Class<?> wildcardClass1 = whoClient0.getClass();
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+
+    @Test
+    public void test2() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test2");
+        java.lang.Object obj0 = new java.lang.Object();
+        java.lang.Class<?> wildcardClass1 = obj0.getClass();
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+}
+
