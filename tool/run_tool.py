c80389aa9afde0b65c7a960acf1a9ecc51ae0a00
Kedar <kedar.takwane@gmail.com>
2023-11-23 00:50:14 -0600

    updated run_tool and added methods to run

--- tool/run_tool.py
+++ tool/run_tool.py
@@ -4,6 +4,7 @@ import argparse
 import openai
 import datetime
 import time
+from datetime import date
 import re
 from prompt.tests import Tests
 from helpers.utils import get_metadata, extract_class_from_class_qlf_name
@@ -24,6 +25,8 @@ def chat(i, task, out, meta_data_arr):
     out.append("## Prompt")
     out.append("**" + init + "**")
     idx = 1
+    instruction_str = ""
+    repeat = False
 
     task_prompt = [
         {
@@ -62,12 +65,19 @@ def chat(i, task, out, meta_data_arr):
         elif idx == 2:
             prompt += '\nPARENT_CLASS: \n' + meta_data_arr[idx-1]
             out.append('\nPARENT_CLASS: \n' + meta_data_arr[idx-1])
+            instruction_str += "PARENT_CLASS"
         elif idx == 3:
             prompt += '\nPARAMETERS: \n' + meta_data_arr[idx-1]
             out.append('\nPARAMETERS: \n' + meta_data_arr[idx-1])
+            instruction_str += ", PARAMETERS"
         elif idx == 4:
             prompt += '\nLOCAL_VARIABLES: \n' + meta_data_arr[idx-1]
             out.append('\nLOCAL_VARIABLES: \n' + meta_data_arr[idx-1])
+            instruction_str += ", LOCAL_VARIABLES"
+        elif idx == 5:
+            prompt = prompt.replace("<META-DATA>", instruction_str)
+            if repeat:
+                prompt += "\nGenerate the JUnit of the method completely!"
 
         if idx <= 4:
             prompt += f"\nIf you understand this, say only 'Yes'."
@@ -93,22 +103,35 @@ def chat(i, task, out, meta_data_arr):
         out.append(reply)
         out.append("```")
 
+        if "no" in reply.lower()[:2]:
+            print(f'Retrying as reply returned No')
+            out.append("`**Note:** Retrying as ChatGPT output No`")
+            out.append("---")
+            continue
+
         if idx == 5:
-            task.write_unit_test(i, reply)
+            if not repeat and ("<JunitTest>" not in reply or "</JunitTest>" not in reply):
+                repeat = True
+                out.append(
+                    "`**Note:** Retrying as ChatGPT did not output the entire code`")
+                out.append("---")
+                continue
+            task.write_unit_test(
+                str(i) + "-" + str(int(time.time())), reply)
 
         task_prompt.append({"role": "assistant", "content": reply})
 
         idx += 1
         if not task.parse_response(idx, reply):
-            task.store(i, out)
+            task.store(str(i) + "-" + str(int(time.time())), out)
             break
 
 
-def tests(i, data_json, out, meta_data_arr):
+def tests(i, data_json, out, meta_data_arr, mode_of_exec):
     out.append("# Task: Generate Tests")
     out.append("---")
-    test = Tests(i, data_json)
-    chat(i, test, out, meta_data_arr)
+    test = Tests(i, data_json, mode_of_exec)
+    chat(i, test, out, meta_data_arr, )
 
 
 def read_data(data_path):
@@ -121,10 +144,34 @@ def read_data(data_path):
 
 # Script begins here
 # if __name__ == 'main':
+# Mode 0 : Without Meta-data, Mode 1: With only method's class meta-data, Mode 2: With only method_params,
+# Mode 3: With only method_vars, Mode 4: With all Meta-data
+# Default: Mode 4
+p = argparse.ArgumentParser()
+
+p.add_argument('--mode', dest='mode',
+               default='4', help='Describe the mode of execution')
+
+args = p.parse_args()
+mode_of_exec = args.mode
+
 data_path = "../data/methods.json"
 data_json = read_data(data_path)
+is_first = True
 
-for i in range(len(data_json)):
+if int(mode_of_exec) >= 5:
+    mode_of_exec = '4'
+
+if mode_of_exec == '4':
+    data_points = [i for i in range(len(data_json))]
+else:
+    # Only run for 'medium' type of datapoints
+    data_points = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28]
+
+print(f'>>> Mode of execution: {mode_of_exec}')
+print(f'>>> Datapoints: {data_points}')
+
+for i in data_points:
     out = []
     # Unit test generation
     print(f'Starting UNIT TEST GENERATION task for the object #{i + 1}...')
@@ -138,15 +185,19 @@ for i in range(len(data_json)):
     meta_data_arr = [method_class_name]
     first_ = {
         "qualifiedName": class_qual_name,
-        "variables": class_vars,
-        "methods": class_methods
+        "variables": class_vars if mode_of_exec == '1' or mode_of_exec == '4' else [],
+        "methods": class_methods if mode_of_exec == '1' or mode_of_exec == '4' else []
     }
 
     meta_data_arr.append(f'{method_class_name}: {json.dumps(first_)}')
-    meta_data_arr.append(json.dumps(method_params)
-                         if method_params != "" else "")
-    meta_data_arr.append(json.dumps(method_vars) if method_vars != "" else "")
+    meta_data_arr.append(""
+                         if method_params == "" or mode_of_exec in ['0', '1', '3'] else json.dumps(method_params))
+    meta_data_arr.append("" if method_vars == "" or mode_of_exec in [
+                         '0', '1', '2'] else json.dumps(method_vars))
+    if not is_first:
+        time.sleep(60)
 
-    tests(i+1, data_json[i], out, meta_data_arr)
+    tests(i+1, data_json[i], out, meta_data_arr, mode_of_exec)
+    is_first = False
 
 print(f'Task Completed')
